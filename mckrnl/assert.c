98d64fae4df4ef34751ca0f0304b91be60079a11
Glowman554 <glowman434@gmail.com>
2022-11-04 22:32:31 +0100

    exit task if kernel assertion fails & scheduler is running

--- mckrnl/assert.c
+++ mckrnl/assert.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+#include <scheduler/scheduler.h>
+
 void __assert_fail(const char* __assertion, const char* __file, unsigned int __line) {
-	abortf("Assertion failed: \"%s\" in file %s at line %d\n", __assertion, __file, __line);
+    if (is_scheduler_running) {
+	    printf("Kernel assertion failed: \"%s\" in file %s at line %d\n", __assertion, __file, __line);
+	    exit_task(&tasks[current_task]);
+    } else {
+	    abortf("Assertion failed: \"%s\" in file %s at line %d\n", __assertion, __file, __line);
+    }
 }
