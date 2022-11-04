7d956b25594b15989c25b66025e0dabd94895bea
Glowman554 <glowman434@gmail.com>
2022-11-04 22:59:06 +0100

    fix dsdt potentially not being mapped

--- mckrnl/driver/acpi/dsdt.c
+++ mckrnl/driver/acpi/dsdt.c
@@ -25,6 +25,12 @@ void dsdt_init() {
 	uint8_t *s5_addr = (uint8_t*) dsdt_addr + 36;
 	uint32_t dsdt_length = ((sdt_header_t*) dsdt_addr)->length;
 
+	uint32_t aligned_dsdt_addr = ALIGN_PAGE_DOWN((uintptr_t) dsdt_addr);
+	for (int i = 0; i < dsdt_length / 0x1000; i++) {
+		debugf("mapping dsdt at %x", aligned_dsdt_addr + i * 0x1000);
+		vmm_map_page(kernel_context, aligned_dsdt_addr + i * 0x1000, aligned_dsdt_addr + i * 0x1000, PTE_PRESENT | PTE_WRITE);
+	}
+
 	dsdt_addr *= 2;
 	while (dsdt_length-- > 0) {
 		if (!memcmp(s5_addr, "_S5_", 4)) break;
