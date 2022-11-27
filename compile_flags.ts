fdcb021437009d26a95b8948aa7e0a63558892b9
Glowman554 <glowman434@gmail.com>
2022-11-27 15:27:30 +0100

    small fix

--- compile_flags.ts
+++ compile_flags.ts
@@ -1,6 +1,7 @@
 for(let i = 0; i < Deno.args.length; i++) {
 	if (Deno.args[i + 1] && !Deno.args[i + 1].startsWith("-")) {
-		console.log(Deno.args[i] + " " + Deno.args[i + 1]);
+		console.log(Deno.args[i] + Deno.args[i + 1]);
+		i++;
 	} else {
 		console.log(Deno.args[i]);
 	}
