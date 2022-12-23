0668f4e81a155645b23cb41b773281bb99a72c29
Glowman554 <glowman434@gmail.com>
2022-12-23 21:01:18 +0100

    better init process

--- user/init/main.c
+++ user/init/main.c
@@ -1,33 +1,81 @@
 #include <stdio.h>
 #include <sys/spawn.h>
 #include <sys/env.h>
-
+#include <string.h>
 #include <stddef.h>
+#include <stdlib.h>
 
-#define ROOT_FS "initrd:/"
-#define TERMINAL "initrd:/bin/terminal.elf"
+// #define ROOT_FS "initrd:/"
+#define TERMINAL "bin/terminal.elf"
 
-char* envp[] = {
-	"ROOT=initrd:",
-	"PATH=initrd:/bin",
-	NULL
-};
+// char* envp[] = {
+// 	"ROOT=initrd:",
+// 	"PATH=initrd:/bin",
+// 	NULL
+// };
+
+char* copy_until(char until, char* src, char* dest) {
+	int i = 0;
+	while (src[i] != '\0' && src[i] != until) {
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+
+	return &src[i + (src[i] == '\0' ? 0 : 1)];
+}
+
+char* child_envp[32] = { NULL };
+void envp_append(char* key, char* val) {
+	printf("Appending %s=%s to envp...\n", key, val);
+	
+	int sz = strlen(key) + strlen(val) + 2;
+	char* buf = malloc(sz);
+	memset(buf, 0, sz);
+
+	strcat(buf, key);
+	strcat(buf, "=");
+	strcat(buf, val);
+
+	for (int i = 0; i < 32; i++) {
+		if (child_envp[i] == NULL) {
+			child_envp[i] = buf;
+			return;
+		}
+	}
+
+	abort();
+}
 
 int main(int argc, char* argv[]) {
-	char* cwd = "initrd:/";
+	char cwd[64] = { 0 };
+	copy_until(':', argv[0], cwd);
+	strcat(cwd, ":/");
+	printf("got cwd %s\n", cwd);
+
 	set_env(SYS_SET_PWD_ID, cwd);
+
+	char terminal[128] = { 0 };
+	strcat(terminal, cwd);
+	strcat(terminal, TERMINAL);
+	printf("got terminal %s\n", terminal);
+
+	char path[128] = { 0 };
+	strcat(path, cwd);
+	strcat(path, "bin");
+	envp_append("PATH", path);
 
 	char* autostart = "startup.msh";
 	FILE* f = fopen(autostart, "r");
 	if (f) {
 		fclose(f);
 		char* new_argv[] = {
-			TERMINAL,
+			terminal,
 			autostart,
 			NULL
 		};
 
-		int pid = spawn(new_argv[0], (const char**) new_argv, (const char**) envp);
+		int pid = spawn(new_argv[0], (const char**) new_argv, (const char**) child_envp);
 
 		while (get_proc_info(pid)) {
 			yield();
@@ -37,12 +85,12 @@ int main(int argc, char* argv[]) {
 	}
 
 	char* new_argv[] = {
-		TERMINAL,
+		terminal,
 		NULL
 	};
 
 	while (true) {
-		int pid = spawn(new_argv[0], (const char**) new_argv, (const char**) envp);
+		int pid = spawn(new_argv[0], (const char**) new_argv, (const char**) child_envp);
 
 		while (get_proc_info(pid)) {
 			yield();
