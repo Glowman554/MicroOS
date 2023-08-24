d4cad0455b6e2b24750234cd028f613a3a819b1d
Glowman554 <glowman434@gmail.com>
2023-08-24 23:38:59 +0200

    include visual emulator in phoenix v2 port

--- foxtail/phoenixv2_tools/build.sh
+++ foxtail/phoenixv2_tools/build.sh
@@ -6,6 +6,7 @@ else
     (
         cd src
         git am < ../0001-fix-disasm.patch
+        git am < ../0002-fix-visual-emulator.patch
     )
 fi
 
@@ -17,6 +18,7 @@ fi
 make -f $1 -C src/tools/assemblerv2 PROGRAM=phoenixv2-as.elf LIBS_ZIP=$2 USER_CFLAGS=-I../common extract_libs prog
 make -f $1 -C src/tools/disassembler PROGRAM=phoenixv2-disasm.elf LIBS_ZIP=$2 USER_CFLAGS=-I../common extract_libs prog
 make -f $1 -C src/tools/emulator PROGRAM=phoenixv2-emu.elf LIBS_ZIP=$2 USER_CFLAGS=-I../common extract_libs prog
+make -f $1 -C src/tools/visual_eumlator PROGRAM=phoenixv2-emu-visual.elf LIBS_ZIP=$2 USER_CFLAGS=-I../common extract_libs prog
 
 bash ../run_foxtail.sh $1 $2 $3
 cp -v foxtail/install.elf ../pkgs/phoenixv2_tools_install.elf
