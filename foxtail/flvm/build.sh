c03ce787db26b82ddb79a6523fb91a1dba27161b
Glowman554 <glowman434@gmail.com>
2024-01-23 12:08:07 +0100

    oops

--- foxtail/flvm/build.sh
+++ foxtail/flvm/build.sh
@@ -4,10 +4,6 @@ fi
 
 make -f $1 -C src PROGRAM=flvm.elf LIBS_ZIP=$2 extract_libs prog
 
-(
-	#cd example
-	#flp build
-)
 
 bash ../run_foxtail.sh $1 $2 $3
 cp -v foxtail/install.elf ../pkgs/flvm_install.elf
