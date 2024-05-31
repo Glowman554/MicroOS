0e1d6343b5bd676e29198ef342e66144e5b2634c
Glowman554 <glowman434@gmail.com>
2024-05-31 19:37:49 +0200

    fix test runner and update copyright

--- test/download.sh
+++ test/download.sh
@@ -1,5 +1,4 @@
-wget https://cdn.discordapp.com/attachments/805055812376330241/1164646529009725440/libv86.js -O libv86.js
-wget https://cdn.discordapp.com/attachments/805055812376330241/1164646529718566942/v86.wasm -O v86.wasm
+wget https://github.com/copy/v86/releases/download/latest/libv86.js -O libv86.js
+wget https://github.com/copy/v86/releases/download/latest/v86.wasm -O v86.wasm
 wget https://github.com/Glowman554/NextOS/raw/master/os-test/seabios.bin -O seabios.bin
 wget https://github.com/Glowman554/NextOS/raw/master/os-test/vgabios.bin -O vgabios.bin
-
