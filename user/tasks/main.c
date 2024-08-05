d953de9d1dde43bd3688073f933577742e07c1c0
Glowman554 <glowman434@gmail.com>
2024-08-06 01:34:38 +0200

    improve task manager and fix pinning

--- user/tasks/main.c
+++ user/tasks/main.c
@@ -7,6 +7,51 @@
 #include <string.h>
 
 #define MAX_TASKS 32
+#define MAX(a, b) ((a > b) ? a : b)
+
+int render_pids(task_list_t* list, int amount) {
+	draw_string(0, 1, "pid", FOREGROUND_WHITE | BACKGROUND_BLACK);
+
+	int max = 0;
+	for (int i = 0; i < amount; i++) {
+		char buf[16] = { 0 };
+		int l = sprintf(buf, "%d", list[i].pid);
+		if (l > max) {
+			max = l;
+		}
+		draw_string(0, i + 2, buf, FOREGROUND_WHITE | BACKGROUND_BLACK);
+	}
+	return MAX(max, 3);
+}
+
+int render_terms(task_list_t* list, int amount, int offset) {
+	draw_string(offset, 1, "term", FOREGROUND_WHITE | BACKGROUND_BLACK);
+
+	int max = 0;
+	for (int i = 0; i < amount; i++) {
+		char buf[16] = { 0 };
+		int l = sprintf(buf, "%d", list[i].term);
+		if (l > max) {
+			max = l;
+		}
+		draw_string(offset, i + 2, buf, FOREGROUND_WHITE | BACKGROUND_BLACK);
+	}
+	return MAX(max, 4);
+}
+
+int render_names(task_list_t* list, int amount, int offset) {
+	draw_string(offset, 1, "name", FOREGROUND_WHITE | BACKGROUND_BLACK);
+	for (int i = 0; i < amount; i++) {
+		draw_string(offset, i + 2, list[i].name, FOREGROUND_WHITE | BACKGROUND_BLACK);
+	}
+}
+
+
+void render_separator(int amount, int offset) {
+	for (int i = 0; i < amount; i++) {
+		draw_string(offset, i + 2, "|", FOREGROUND_WHITE | BACKGROUND_BLACK);
+	}
+}
 
 int main(int argc, char* argv[]) {
 	task_list_t* list = malloc(sizeof(task_list_t) * MAX_TASKS);
@@ -25,12 +70,14 @@ int main(int argc, char* argv[]) {
 		draw_string(get_width() / 2 - 6, 0, "MicroOS Tasks", FOREGROUND_CYAN | BACKGROUND_BLACK);
 
 		int amount = get_task_list(list, MAX_TASKS);
-		for (int i = 0; i < amount; i++) {
-			char buf[16] = { 0 };
-			sprintf(buf, "%d", list[i].pid);
-			draw_string(0, i + 2, buf, FOREGROUND_WHITE | BACKGROUND_BLACK);
-			draw_string(5, i + 2, list[i].name, FOREGROUND_WHITE | BACKGROUND_BLACK);
-		}
+
+		int max = render_pids(list, amount);
+		render_separator(amount, ++max);
+		max += 2;
+		max += render_terms(list, amount, max);
+		render_separator(amount, ++max);
+		max += 2;
+		render_names(list, amount, max);
 
 		draw_string(0, get_height() - 1, "kill > ", FOREGROUND_DARKGRAY | BACKGROUND_BLACK);
 
