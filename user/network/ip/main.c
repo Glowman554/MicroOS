f5254659ed1cb9129324a04c29284a9b06182042
Glowman554 <vossjanick62@gmail.com>
2026-06-08 09:39:26 +0200

    added new ip setup command

--- user/network/ip/main.c
+++ user/network/ip/main.c
@@ -5,6 +5,16 @@
 #include <non-standard/net/nettools.h>
 #include <non-standard/net/ipv4.h>
 
+void show(int nic_id, nic_content_t* nic_config) {
+    printf("interface %d\n", nic_id);
+    printf("\taddress: %d.%d.%d.%d\n", nic_config->ip_config.ip.ip_p[0], nic_config->ip_config.ip.ip_p[1], nic_config->ip_config.ip.ip_p[2], nic_config->ip_config.ip.ip_p[3]);
+    printf("\tsubnet: %d.%d.%d.%d\n", nic_config->ip_config.subnet_mask.ip_p[0], nic_config->ip_config.subnet_mask.ip_p[1], nic_config->ip_config.subnet_mask.ip_p[2], nic_config->ip_config.subnet_mask.ip_p[3]);
+    printf("\tgateway: %d.%d.%d.%d\n", nic_config->ip_config.gateway_ip.ip_p[0], nic_config->ip_config.gateway_ip.ip_p[1], nic_config->ip_config.gateway_ip.ip_p[2], nic_config->ip_config.gateway_ip.ip_p[3]);
+    printf("\tdns: %d.%d.%d.%d\n", nic_config->ip_config.dns_ip.ip_p[0], nic_config->ip_config.dns_ip.ip_p[1], nic_config->ip_config.dns_ip.ip_p[2], nic_config->ip_config.dns_ip.ip_p[3]);
+    printf("\tmac: %x:%x:%x:%x:%x:%x\n", nic_config->mac.mac_p[0], nic_config->mac.mac_p[1], nic_config->mac.mac_p[2], nic_config->mac.mac_p[3], nic_config->mac.mac_p[4], nic_config->mac.mac_p[5]);
+    printf("\tdevice: %s\n",nic_config->name);
+}
+
 int main(int argc, char* argv[]) {
     // EXAMPLE:
     // ip address 10.0.2.15
@@ -13,11 +23,16 @@ int main(int argc, char* argv[]) {
     // ip dns 10.0.2.3
     // ip show
     // ip -n 2 address 10.0.2.2
+    // ip setup 10.0.2.15 255.255.255.0 10.0.2.2 10.0.2.3
 
     int nic_id = 0;
 
     char* command = NULL;
     char* ipStr = NULL;
+
+    char* ipSetupSubnetStr = NULL;
+    char* ipSetupGatewayStr = NULL;
+    char* ipSetupDnsStr = NULL;
     
     int idx = 1;
 	while (idx < argc) {
@@ -37,12 +52,22 @@ int main(int argc, char* argv[]) {
             printf("\tip gateway <ip>\n");
             printf("\tip dns <ip>\n");
             printf("\tip show\n");
+            printf("\tip setup <ip> <subnet> <gateway> <dns>\n");
 			exit(0);
 		} else {
             if (command == NULL) {
                 command = argv[idx];
             } else if (ipStr == NULL) {
                 ipStr = argv[idx];
+            } else if (ipSetupSubnetStr == NULL) {
+                ipSetupSubnetStr = argv[idx];
+            } else if (ipSetupGatewayStr == NULL) {
+                ipSetupGatewayStr = argv[idx];
+            } else if (ipSetupDnsStr == NULL) {
+                ipSetupDnsStr = argv[idx];
+            } else {
+                printf("Error: too many arguments\n");
+                abort();
             }
         }
 		idx++;
@@ -53,13 +78,7 @@ int main(int argc, char* argv[]) {
     nic_content_t nic_config = nic_read(nic_id);
 
     if (strcmp(command, "show") == 0) {
-        printf("interface %d\n", nic_id);
-        printf("\taddress: %d.%d.%d.%d\n", nic_config.ip_config.ip.ip_p[0], nic_config.ip_config.ip.ip_p[1], nic_config.ip_config.ip.ip_p[2], nic_config.ip_config.ip.ip_p[3]);
-        printf("\tsubnet: %d.%d.%d.%d\n", nic_config.ip_config.subnet_mask.ip_p[0], nic_config.ip_config.subnet_mask.ip_p[1], nic_config.ip_config.subnet_mask.ip_p[2], nic_config.ip_config.subnet_mask.ip_p[3]);
-        printf("\tgateway: %d.%d.%d.%d\n", nic_config.ip_config.gateway_ip.ip_p[0], nic_config.ip_config.gateway_ip.ip_p[1], nic_config.ip_config.gateway_ip.ip_p[2], nic_config.ip_config.gateway_ip.ip_p[3]);
-        printf("\tdns: %d.%d.%d.%d\n", nic_config.ip_config.dns_ip.ip_p[0], nic_config.ip_config.dns_ip.ip_p[1], nic_config.ip_config.dns_ip.ip_p[2], nic_config.ip_config.dns_ip.ip_p[3]);
-        printf("\tmac: %x:%x:%x:%x:%x:%x\n", nic_config.mac.mac_p[0], nic_config.mac.mac_p[1], nic_config.mac.mac_p[2], nic_config.mac.mac_p[3], nic_config.mac.mac_p[4], nic_config.mac.mac_p[5]);
-        printf("\tdevice: %s\n",nic_config.name);
+        show(nic_id, &nic_config);
     } else {
         assert(ipStr != NULL);
         ip_u ip = parse_ip(ipStr);
@@ -72,6 +91,16 @@ int main(int argc, char* argv[]) {
             nic_config.ip_config.gateway_ip = ip;
         } else if (strcmp(command, "dns") == 0) {
             nic_config.ip_config.dns_ip = ip;
+        } else if (strcmp(command, "setup") == 0) {
+            assert(ipSetupSubnetStr != NULL);
+            assert(ipSetupGatewayStr != NULL);
+            assert(ipSetupDnsStr != NULL);
+            nic_config.ip_config.ip = ip;
+            nic_config.ip_config.subnet_mask = parse_ip(ipSetupSubnetStr);
+            nic_config.ip_config.gateway_ip = parse_ip(ipSetupGatewayStr);
+            nic_config.ip_config.dns_ip = parse_ip(ipSetupDnsStr);
+
+            show(nic_id, &nic_config);
         } else {
             printf("Unknown command: %s\n", command);
             abort();
